be7625c4c257cc589291ba592768311871a495fd
Lorax34 <ivanalejandroperezjimenez#@gmail.com>
2025-05-03 11:42:42 -0400

    LlamadaFactory arreglado

--- database/factories/LlamadaFactory.php
+++ database/factories/LlamadaFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Carbon\Carbon;
+use App\Models\Cliente;
 
 class LlamadaFactory extends Factory
 {
@@ -18,11 +19,32 @@ class LlamadaFactory extends Factory
         $hora = Carbon::parse($fechaHora)->hour;
         $nocturna = $hora >= 23 || $hora < 5;
 
+        // Obtener un cliente aleatorio para el número de origen
+        $clienteOrigen = Cliente::inRandomOrder()->first();
+
+        // Si no hay clientes, crear uno nuevo
+        if (!$clienteOrigen) {
+            $clienteOrigen = Cliente::factory()->create();
+        }
+
+        // Para destino nacional, obtener otro cliente diferente al de origen
+        $clienteDestinoNacional = null;
+        if ($esNacional) {
+            $clienteDestinoNacional = Cliente::where('id', '!=', $clienteOrigen->id)
+                ->inRandomOrder()
+                ->first();
+
+            // Si no hay otro cliente, crear uno nuevo
+            if (!$clienteDestinoNacional) {
+                $clienteDestinoNacional = Cliente::factory()->create();
+            }
+        }
+
         return [
             'minutos' => $esServicio ? 0 : $this->faker->numberBetween(1, 60),
             'fecha_hora' => $fechaHora,
-            'numero_origen' => $this->faker->numerify('5#######'), // Número cubano
-            'numero_destino' => $this->generateNumeroDestino($esInternacional, $esNacional, $esServicio),
+            'numero_origen' => $clienteOrigen->telefono, // Usar teléfono del cliente origen
+            'numero_destino' => $this->generateNumeroDestino($esInternacional, $esNacional, $esServicio, $clienteDestinoNacional),
             'es_internacional' => $esInternacional,
             'es_nacional' => $esNacional,
             'es_local' => $esLocal,
@@ -31,13 +53,14 @@ class LlamadaFactory extends Factory
             'cod_local_dest' => $esNacional ? $this->faker->numerify('##') : null,
             'cod_pais_dest' => $esInternacional ? $this->faker->numerify('##') : null, // Código país 2 dígitos
             'tarifa_pais' => $esInternacional ? $this->faker->randomFloat(2, 0.10, 5.00) : null,
+            'cliente_id' => $clienteOrigen->id, // Relación con el cliente
             'precio' => function (array $attributes) use ($nocturna) {
                 return $this->calculatePrecio($attributes, $nocturna);
             },
         ];
     }
 
-    private function generateNumeroDestino($esInternacional, $esNacional, $esServicio)
+    private function generateNumeroDestino($esInternacional, $esNacional, $esServicio, $clienteDestinoNacional = null)
     {
         if ($esServicio) {
             return '*-' . $this->faker->numerify('###-####'); // Formato *-077-2244
@@ -48,11 +71,14 @@ class LlamadaFactory extends Factory
         }
 
         if ($esNacional) {
-            return $this->faker->numerify('###-#######'); // Nacional con código localidad
+            // 50% de usar número de cliente destino o generar uno aleatorio
+            return $this->faker->boolean(50)
+                ? $clienteDestinoNacional->telefono
+                : $this->faker->numerify('###-5#######'); // Nacional con código localidad
         }
 
-        // Local: 50% probabilidad de ser a línea arrendada (777777)
-        return $this->faker->boolean(5) ? '777777' : $this->faker->numerify('#######');
+        // Local: 5% probabilidad de ser a línea arrendada (777777)
+        return $this->faker->boolean(5) ? '777777' : $this->faker->numerify('5#######');
     }
 
     private function calculatePrecio($attributes, $nocturna)
